8b7e7a01ab6a24c974f953ff9af38cc5e5f0c709
Frederic Duby <fcd1@columbia.edu>
2015-12-02 15:04:23 -0500

    Use default for width of CUL banner

--- common/footer.php
+++ common/footer.php
@@ -7,7 +7,7 @@
     <script type="text/javascript">
     <!-- 
       CULh_style = 'staticlink'; // staticlink, static, search, standard (default)
-      CULh_width = ''; // fixed, fluid (default)
+      //CULh_width = ''; // fixed, fluid (default)
       //CULh_colorfg = '#333333'; // topnavbar foreground color. hex value. ex: #002B7F
       //CULh_colorbg = '#666666'; // topnavbar background color. hex value. ex: #779BC3
       //CULh_nobs = 1; // uncomment to NOT load our bootstrap javascript file and or use your own (v2.3.x required)
